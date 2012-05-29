1d0cd9edf93d62bda5f04051c7de674081d6dab9
Steve Streza <steve@readitlater.com>
2012-05-29 14:17:41 -0700

    Initial commit

---
+++ SDK/PocketAPI.h
@@ -0,0 +1,13 @@
+//
+//  PocketAPI.h
+//  PocketSDK
+//
+//  Created by Steve Streza on 5/29/12.
+//  Copyright (c) 2012 Read It Later, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PocketAPI : NSObject
+
+@end
---
+++ SDK/PocketAPI.m
@@ -0,0 +1,13 @@
+//
+//  PocketAPI.m
+//  PocketSDK
+//
+//  Created by Steve Streza on 5/29/12.
+//  Copyright (c) 2012 Read It Later, Inc. All rights reserved.
+//
+
+#import "PocketAPI.h"
+
+@implementation PocketAPI
+
+@end
