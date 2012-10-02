84045a9b0e02c6cf8c40f9915badcc6d030a8210
Steve Streza <steve@readitlater.com>
2012-10-02 16:59:08 -0700

    1.0rc1

--- SDK/PocketAPI.m
+++ SDK/PocketAPI.m
@@ -29,7 +29,7 @@
 #import <sys/sysctl.h>
 #import <CommonCrypto/CommonDigest.h>
 
-#define POCKET_SDK_VERSION @"0.9.2"
+#define POCKET_SDK_VERSION @"1.0rc1"
 
 static NSString *kPocketAPICurrentLoginKey = @"PocketAPICurrentLogin";
 
